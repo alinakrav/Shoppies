5c33c52134f579a127d4c227079e1978e0275ebd
kravchenkoalina <alina14022000@gmail.com>
2020-12-28 01:45:17 -0500

    revert to old version

--- index.php
+++ index.php
@@ -35,17 +35,5 @@
             } 
         ?>
         <code id="response" class="container"></code>
-
-        <!-- firebase snippet -->
-        <!-- The core Firebase JS SDK is always required and must be listed first -->
-        <script src="/__/firebase/8.2.1/firebase-app.js"></script>
-
-        <!-- TODO: Add SDKs for Firebase products that you want to use
-            https://firebase.google.com/docs/web/setup#available-libraries -->
-        <script src="/__/firebase/8.2.1/firebase-analytics.js"></script>
-
-        <!-- Initialize Firebase -->
-        <script src="/__/firebase/init.js"></script>
-
     </body>
 </html>
