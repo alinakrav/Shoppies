e6348395ed7246303de780278d33a34b261ae6d8
kravchenkoalina <alina14022000@gmail.com>
2020-12-28 05:57:16 -0500

    got the functionality down, php wasn't needed after all

--- script.js
+++ script.js
@@ -7,14 +7,44 @@ function search() {
     request.onload = function() {
         show_results(request.response);
         return request.response;
-        // will display title of first movie in list
-        // input.placeholder = resp.Search[0].Title;
     }
     request.send();
 }
 
 function show_results(resp) {
-    ul.append("<li>hello</li>");
-    document.getElementById("response").innerHTML = JSON.stringify(resp);
+    // erase old results
+    results.innerHTML = '';
+    let j = 5;
+    for (let i = 0; i < j; i++) {
+        // if wrong index, continue to the next one
+        try {
+            let movie = resp.Search[i]
+            if (movie.Type != "movie") 
+                j++;
+            else
+                // list elements will be id'd as their i index when nominated
+                results.innerHTML += '<li><a id="' + i + '"> ' + movie.Title + ' (' + movie.Year + ') </a><button class="nominate_btn" onclick="nominate(' + i + ')">Nominate</button></li>';
+        } finally { continue; }
+    }
 
+    document.getElementById("response").innerHTML = JSON.stringify(resp);
+}
+
+function nominate(id) {
+    let elem = document.getElementById(id);
+    // delete li element on button click
+    nominations.innerHTML += '<li>' + elem.innerText + ' <button onclick="remove(this, ' + id + ')">Remove</button></li>';
+    // disable button when nominated
+    elem.parentNode.children[1].disabled = true;
+
+    // display banner when 5 nominated
+    if (nominations.children.length == 5)
+        console.log('banner: 5 nominated');
+}
+
+function remove(elem, id) {
+    // remove item from results
+    elem.parentNode.outerHTML = '';
+    // enable button again
+    document.getElementById(id).parentNode.children[1].disabled = false;
 }
