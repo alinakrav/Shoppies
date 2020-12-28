416343a1e84d266dc8855f92e8eca8f8b2b6e5da
kravchenkoalina <alina14022000@gmail.com>
2020-12-28 03:43:27 -0500

    test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <h1>The Shoppies</h1>
 
         <div class="container">
-            <form id="searchform" method="post" onsubmit="search(); return false;">
+            <form id="searchform" method="post" action="" onsubmit="search(); return false;">
                 <label for="searchbar">search</label>
                 <input type="text" id="searchbar" placeholder="search...">
             </form>
